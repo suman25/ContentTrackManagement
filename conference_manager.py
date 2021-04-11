6928a3b844222c643c5c20a49fe11ea1d1b60b6b
Suman Daryani <sumandaryani125@gmail.com>
2021-04-12 02:56:09 +0530

    refactoring: get the details from config, session enum

--- conference_manager.py
+++ conference_manager.py
@@ -1,8 +1,11 @@
 import sys
+
+from conference_config import *
 from task import Task
 import re
 from conference import Conference
-from session import Session
+from session import Session, SessionType
+
 task_list = []
 
 
@@ -11,55 +14,62 @@ class ConferenceManager(object):
         self.input_file = input_file
 
     def read_file_and_create_tasks(self):
-        with open(self.input_file) as file:
-            for line in file:
-                if any(chr.isdigit() for chr in line):
-                    res = re.split('(\d+)', line)
-                else:
-                    index = line.find('lightning')
-                    res[0] = line[0:index-1]
-                    res[1] = 'lightning'
-                task = Task(res[0], res[1])
-                task_list.append(task)
-                task_list.sort(key=lambda task: task.task_minutes, reverse=True)
+            with open(self.input_file) as file:
+                for line in file:
+                    if any(chr.isdigit() for chr in line):
+                        res = re.split('(\d+)', line)
+                    else:
+                        index = line.find('lightning')
+                        res[0] = line[0:index - 1]
+                        res[1] = 'lightning'
+                    task = Task(res[0], res[1])
+                    task_list.append(task)
+                    task_list.sort(key=lambda task: task.task_minutes, reverse=True)
 
     def print_sessions(self, conference):
         for track in conference.tracks:
-            print('Track'+ str(track.track_id))
+            print('Track' + str(track.track_id))
             for session in track.sessions:
                 for slot in session.session_slots:
-                    print(slot.slot_time.strftime("%I:%M %p") , slot.slot_name , str(slot.slot_duration) + 'min')
+                    print(slot.slot_time.strftime("%I:%M %p"), slot.slot_name, str(slot.slot_duration) + 'min')
 
     def run(self):
+        session_list = []
         self.read_file_and_create_tasks()
         if len(task_list) > 0:
             conference = Conference()
             while any(task.is_scheduled == 0 for task in task_list):
-                morning_session = Session('Morning_Session', [9, 0, 0], 180)
+                morning_session = Session(SessionType.Morning, MORNING_START_TIME, MORNING_SESSION_DURATION)
                 self.fill_session(morning_session)
-                lunch_session = Session('Lunch Session', [12, 0, 0], 60)
-                lunch_session.add_slot('Lunch', 60)
-                noon_session = Session('Noon_Session', [13, 0, 0], 240)
+                session_list.append(morning_session)
+                lunch_session = Session(SessionType.Lunch, LUNCH_START_TIME, LUNCH_SESSION_DURATION)
+                lunch_session.add_slot('Lunch', LUNCH_SLOT_DURATION)
+                session_list.append(lunch_session)
+                noon_session = Session(SessionType.Noon, NOON_START_TIME, NOON_SESSION_DURATION)
                 self.fill_session(noon_session)
-                networking_session = Session('Networking Session',[noon_session.next_slot_time.hour , noon_session.next_slot_time.minute , noon_session.next_slot_time.second] , 60)
-                networking_session.add_slot('Networking',60)
-                conference.add_track()
-                current_track = conference.get_current_track()
-                current_track.add_sessions(morning_session)
-                current_track.add_sessions(lunch_session)
-                current_track.add_sessions(noon_session)
-                current_track.add_sessions(networking_session)
+                session_list.append(noon_session)
+                networking_start_time = noon_session.next_slot_time
+                networking_session = Session(SessionType.Networking,
+                                             [networking_start_time.hour, networking_start_time.minute,
+                                              networking_start_time.second], NETWORKING_SESSION_DURATION)
+                networking_session.add_slot('Networking', NETWORKING_SLOT_DURATION)
+                session_list.append(networking_session)
+                self.create_track_and_fill_track(conference, session_list)
             self.print_sessions(conference)
 
+    def create_track_and_fill_track(self, conference, session_list):
+        conference.add_track()
+        current_track = conference.get_current_track()
+        for session in session_list:
+            current_track.add_sessions(session)
 
     def fill_session(self, session):
-        for index,task_element in enumerate(task_list):
-            if not(task_element.is_scheduled) and session.has_room(task_element.task_minutes):
+        for index, task_element in enumerate(task_list):
+            if not (task_element.is_scheduled) and session.has_room(task_element.task_minutes):
                 session.add_slot(task_element.task_name, task_element.task_minutes)
                 task_element.is_scheduled = 1
             if session.is_session_full:
                 break
-
 
 
 if __name__ == '__main__':
--- session.py
+++ session.py
@@ -1,11 +1,11 @@
 import datetime
 from slot import Slot
-
+from enum import Enum
 
 class Session(object):
 
-    def __init__(self, session_name, session_start_time, session_duration):
-        self.session_name = session_name
+    def __init__(self, session_type, session_start_time, session_duration):
+        self.session_type = session_type
         self.session_start_time = datetime.time(session_start_time[0], session_start_time[1], session_start_time[2])
         self.next_slot_time = datetime.time(session_start_time[0], session_start_time[1], session_start_time[2])
         self.session_duration = session_duration
@@ -27,3 +27,9 @@ class Session(object):
         self.next_slot_time = datetime.datetime.combine(datetime.date(1,1,1),self.next_slot_time) + datetime.timedelta(minutes= slot_duration)
         self.session_filled = self.session_filled + slot_duration
         self.next_slot_time = self.next_slot_time.time()
+
+class SessionType(Enum):
+    Morning = 1
+    Lunch = 2
+    Noon = 3
+    Networking = 4
